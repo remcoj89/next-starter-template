fc69b406f01aa7e54fa88462f9580ef951cc6367
remco jonk <remcojonk@hotmail.com>
2024-05-29 18:45:03 +0200

    initiate next js starter template with standard css variables and a responsive navbar

---
+++ ui/components/nav-component/nav-bar.component.jsx
@@ -0,0 +1,40 @@
+'use client';
+// React Hooks
+import {useState} from 'react';
+import { usePathname } from 'next/navigation';
+
+// Styling
+import Styles from './nav-bar.module.css';
+import Link from 'next/link';
+
+// Components
+import BurgerMenu from '@/assets/icon-burger-menu/icon-burger-menu.component';
+
+const Navbar = () => {
+const screenWidth = window.screen.width;
+const pathname = usePathname()
+const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+// Toggle burger menu
+const toggleBurgerMenu = () => { setIsMenuOpen(!isMenuOpen)}
+
+  return (
+  <>
+    <nav className={Styles.navbar}>
+      <div className="wrapper">
+        <div className={Styles.navContainer}>
+          <Link href="/">WESTEND DIGITAL</ Link>
+          <div className={Styles.navlinks} style={isMenuOpen && screenWidth <= 478 || screenWidth >= 478 ? {display: "flex"} : {display: "none"}}>
+            <li className={`${Styles.navLink} ${pathname === '/' ? `${Styles.active}` : ""}`} ><Link href="/">home</Link></li>
+            <li className={`${Styles.navLink} ${pathname === '/about' ? `${Styles.active}` : ""}`} ><Link href="/about">about us</Link></li>
+            <li className={`${Styles.navLink} ${pathname === '/contact' ? `${Styles.active}` : ""}`} ><Link href="/plan">contact</Link></li>
+          </div>
+          <BurgerMenu toggleBurgerMenu={toggleBurgerMenu} isMenuOpen={isMenuOpen}/>
+        </div>
+      </div>
+    </nav>
+  </>
+  )
+}
+
+export default Navbar
