e70795bf03723158380b6629c9a5fa477b9fb6bd
remco jonk <remcojonk@hotmail.com>
2024-06-02 16:39:21 +0200

    refactored some logic in the navbar

--- ui/components/nav-component/nav-bar.component.jsx
+++ ui/components/nav-component/nav-bar.component.jsx
@@ -26,7 +26,7 @@ const toggleBurgerMenu = () => { setIsMenuOpen(!isMenuOpen)}
       <div className="wrapper">
         <div className={Styles.navContainer}>
           <Link href="/">WESTEND DIGITAL</ Link>
-          <div className={Styles.navlinks} style={isMenuOpen && screenWidth <= 478 || screenWidth >= 478 ? {display: "flex"} : {display: "none"}}>
+          <div className={Styles.navlinks} style={isMenuOpen || screenWidth >= 478 ? {display: "flex"} : {display: "none"}}>
             <li className={`${Styles.navLink} ${pathname === '/' ? `${Styles.active}` : ""}`} ><Link href="/">home</Link></li>
             <li className={`${Styles.navLink} ${pathname === '/about' ? `${Styles.active}` : ""}`} ><Link href="/about">about us</Link></li>
             <li className={`${Styles.navLink} ${pathname === '/contact' ? `${Styles.active}` : ""}`} ><Link href="/plan">contact</Link></li>
