fcdbbac043f114d2821ad9e91c3b655e555b7899
remco jonk <remcojonk@hotmail.com>
2024-06-19 15:09:34 +0200

    added google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 // Styling
 import { openSans } from "@/utils/fonts/fonts";
 import "./globals.css";
+import GoogleAnalytics from "@/lib/google-analytics/googleAnalytics";
+import CookieBanner from "@/ui/components/cookie-banner/cookie-banner.component";
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={openSans.className}>
+        <GoogleAnalytics GA_MEASUREMENT_ID={process.env.GA_MEASUREMENT_ID} />
         {children}
+        <CookieBanner />
       </body>
     </html>
   );
